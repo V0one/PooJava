55facab5349a5cdc16fe658c275f242ee014d419
V0one <enzogeraldesmartinho@gmail.com>
2025-02-05 15:56:37 +0100

    Finish the US4 with the binary search

--- Pharmacie.java
+++ Pharmacie.java
@@ -19,4 +19,31 @@ public class Pharmacie {
             System.out.println(iterator.next().toString());
         }
     }
+
+    public int afficherProduit (String nom) {
+        nom = nom.toUpperCase();
+        char premierChar = nom.charAt(0);
+        int debut  = 0;
+        int fin  = produits.size() - 1;
+        
+        while (debut < fin) {
+            int millieu = produits.size() / 2;
+
+            if (premierChar == produits.get(millieu).getNom().toUpperCase().charAt(0)) {
+                return produits.get(millieu).getQuantite();
+            }
+            else if (premierChar < produits.get(millieu).getNom().toUpperCase().charAt(0)) {
+                fin = millieu - 1;
+            }
+            else {
+                debut = millieu + 1;
+            }
+        }
+        System.out.println("Erreur de la produit, il n'existe pas");
+        return -1;
+
+
+
+    }
+
 }
